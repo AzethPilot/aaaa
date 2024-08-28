41db90141a13da1df4e6beb657282073de91317f
AzethPilot <diegoandresleones358@gmail.com>
2024-08-29 01:24:35 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,16 @@
+module.exports = {
+  
+  client: {
+    intents: 32767, /////32767 /////3839
+    failIfNotExists: false
+  },
+
+  testing: false,
+  
+  guildId: '1048772624127365190',
+
+  
+  developers: ['735311487463325827']
+}
+
+/////////
